1b230648c5a0ffc42a33c9f524c33a444ebafb98
MehdiYsl <mehdi.yansli@gmail.com>
2023-01-31 14:06:11 +0100

    Ajout du html avce les 4 select

---
+++ resistron.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resistron</title>
+</head>
+<body>
+<label for="color-select1">Choisissez les couleurs correspondantes :</label>
+
+<select name="color" id="color-select1">
+    <option value="">-- Anneau 1 --</option>
+    <option value="noir">Noir</option>
+    <option value="brun">Brun</option>
+    <option value="rouge">Rouge</option>
+    <option value="orange">Orange</option>
+    <option value="jaune">Jaune</option>
+    <option value="vert">Vert</option>
+    <option value="bleu">Bleu</option>
+    <option value="violet">Violet</option>
+    <option value="gris">Gris</option>
+    <option value="blanc">Blanc</option>
+    <option value="or">Or</option>
+    <option value="argent">Argent</option>
+</select>
+
+<select name="" id="color-select2">
+    <option value="">-- Anneau 2 --</option>
+    <option value="noir">Noir</option>
+    <option value="brun">Brun</option>
+    <option value="rouge">Rouge</option>
+    <option value="orange">Orange</option>
+    <option value="jaune">Jaune</option>
+    <option value="vert">Vert</option>
+    <option value="bleu">Bleu</option>
+    <option value="violet">Violet</option>
+    <option value="gris">Gris</option>
+    <option value="blanc">Blanc</option>
+    <option value="or">Or</option>
+    <option value="argent">Argent</option>
+</select>
+<select name="" id="color-select3">
+    <option value="">-- Anneau 3--</option>
+    <option value="noir">Noir</option>
+    <option value="brun">Brun</option>
+    <option value="rouge">Rouge</option>
+    <option value="orange">Orange</option>
+    <option value="jaune">Jaune</option>
+    <option value="vert">Vert</option>
+    <option value="bleu">Bleu</option>
+    <option value="violet">Violet</option>
+    <option value="gris">Gris</option>
+    <option value="blanc">Blanc</option>
+    <option value="or">Or</option>
+    <option value="argent">Argent</option>
+</select>
+<select name="" id="color-select4">
+    <option value="">-- Anneau 4--</option>
+    <option value="noir">Noir</option>
+    <option value="brun">Brun</option>
+    <option value="rouge">Rouge</option>
+    <option value="orange">Orange</option>
+    <option value="jaune">Jaune</option>
+    <option value="vert">Vert</option>
+    <option value="bleu">Bleu</option>
+    <option value="violet">Violet</option>
+    <option value="gris">Gris</option>
+    <option value="blanc">Blanc</option>
+    <option value="or">Or</option>
+    <option value="argent">Argent</option>
+</select>
+</body>
+</html>
